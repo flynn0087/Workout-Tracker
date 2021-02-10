f0c7913ce9f1d0bf60292e974a1a5294bda1b729
Ben Flynn <flynn0087@gmail.com>
2021-02-10 11:39:28 -0600

    post route added to api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const db = require("../models");
 
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then((workout) => {
+            res.status(201).json(workout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+
 module.exports = router;
