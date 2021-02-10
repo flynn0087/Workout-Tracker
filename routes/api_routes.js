ecbef229f4c3598e652e0bc2daf0d932091fd7b1
Ben Flynn <flynn0087@gmail.com>
2021-02-10 11:42:08 -0600

    get routes added to api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -11,5 +11,26 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .sort({ date: -1 })
+        .then((workout) => {
+            res.status(200).json(workout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .sort({ date: -1 })
+        .then((workout) => {
+            res.status(200).json(workout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
